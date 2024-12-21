b9290e36374cb656a3e30b2b50f6bc35397b8ad8
Jackson Dhanyel Santin <privacy@users.noreply.github.com>
2024-12-20 23:24:28 -0300

    Feat(main): Atualiza nome da key das imagens da galeria

--- core/const/gallery.js
+++ core/const/gallery.js
@@ -1,89 +1,89 @@
 export const gallery = [
     {
-        link: "/images/gallery/gallery_1.jpg"
+        image: "/images/gallery/gallery_1.jpg"
     },
     {
-        link: "/images/gallery/gallery_2.jpg"
+        image: "/images/gallery/gallery_2.jpg"
     },
     {
-        link: "/images/gallery/gallery_3.jpg"
+        image: "/images/gallery/gallery_3.jpg"
     },
     {
-        link: "/images/gallery/gallery_4.jpg"
+        image: "/images/gallery/gallery_4.jpg"
     },
     {
-        link: "/images/gallery/gallery_5.jpg"
+        image: "/images/gallery/gallery_5.jpg"
     },
     {
-        link: "/images/gallery/gallery_6.jpg"
+        image: "/images/gallery/gallery_6.jpg"
     },
     {
-        link: "/images/gallery/gallery_7.jpg"
+        image: "/images/gallery/gallery_7.jpg"
     },
     {
-        link: "/images/gallery/gallery_8.jpg"
+        image: "/images/gallery/gallery_8.jpg"
     },
     {
-        link: "/images/gallery/gallery_9.jpg"
+        image: "/images/gallery/gallery_9.jpg"
     },
     {
-        link: "/images/gallery/gallery_10.jpg"
+        image: "/images/gallery/gallery_10.jpg"
     },
     {
-        link: "/images/gallery/gallery_11.jpg"
+        image: "/images/gallery/gallery_11.jpg"
     },
     {
-        link: "/images/gallery/gallery_12.jpg"
+        image: "/images/gallery/gallery_12.jpg"
     },
     {
-        link: "/images/gallery/gallery_13.jpg"
+        image: "/images/gallery/gallery_13.jpg"
     },
     {
-        link: "/images/gallery/gallery_14.jpg"
+        image: "/images/gallery/gallery_14.jpg"
     },
     {
-        link: "/images/gallery/gallery_15.jpg"
+        image: "/images/gallery/gallery_15.jpg"
     },
     {
-        link: "/images/gallery/gallery_16.jpg"
+        image: "/images/gallery/gallery_16.jpg"
     },
     {
-        link: "/images/gallery/gallery_17.jpg"
+        image: "/images/gallery/gallery_17.jpg"
     },
     {
-        link: "/images/gallery/gallery_18.jpg"
+        image: "/images/gallery/gallery_18.jpg"
     },
     {
-        link: "/images/gallery/gallery_19.jpg"
+        image: "/images/gallery/gallery_19.jpg"
     },
     {
-        link: "/images/gallery/gallery_20.jpg"
+        image: "/images/gallery/gallery_20.jpg"
     },
     {
-        link: "/images/gallery/gallery_21.jpg"
+        image: "/images/gallery/gallery_21.jpg"
     },
     {
-        link: "/images/gallery/gallery_22.jpg"
+        image: "/images/gallery/gallery_22.jpg"
     },
     {
-        link: "/images/gallery/gallery_23.jpg"
+        image: "/images/gallery/gallery_23.jpg"
     },
     {
-        link: "/images/gallery/gallery_24.jpg"
+        image: "/images/gallery/gallery_24.jpg"
     },
     {
-        link: "/images/gallery/gallery_25.jpg"
+        image: "/images/gallery/gallery_25.jpg"
     },
     {
-        link: "/images/gallery/gallery_26.jpg"
+        image: "/images/gallery/gallery_26.jpg"
     },
     {
-        link: "/images/gallery/gallery_27.jpg"
+        image: "/images/gallery/gallery_27.jpg"
     },
     {
-        link: "/images/gallery/gallery_28.jpg"
+        image: "/images/gallery/gallery_28.jpg"
     },
     {
-        link: "/images/gallery/gallery_29.jpg"
+        image: "/images/gallery/gallery_29.jpg"
     },
 ]
