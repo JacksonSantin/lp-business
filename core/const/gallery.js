f30a3760b54bbd81a9340a6995074c5eafe06de1
Jackson Dhanyel Santin <jackdhanyelsn@gmail.com>
2024-12-20 22:17:41 -0300

    Feat(main): Atualiza extensão da imagem

--- core/const/gallery.js
+++ core/const/gallery.js
@@ -3,16 +3,16 @@ export const gallery = [
     link: "/images/banner/banner_1.jpg"
   },
   {
-    link: "/images/banner/banner_2.jpg"
+    link: "/images/banner/banner_2.jpeg"
   },
   {
-    link: "/images/banner/banner_3.jpg"
+    link: "/images/banner/banner_3.jpeg"
   },
   {
-    link: "/images/banner/banner_4.jpg"
+    link: "/images/banner/banner_4.jpeg"
   },
   {
-    link: "/images/banner/banner_5.jpg"
+    link: "/images/banner/banner_5.jpeg"
   },
   {
     link: "/images/banner/banner_6.jpg"
